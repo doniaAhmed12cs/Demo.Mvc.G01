1e07657e6ff5de548b065ab95531d99552781674
donia <doniaahmeda902@gmail.com>
2024-09-09 04:42:02 +0300

    first commit

---
+++ Demo.DAL/Modeles/Department.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.DAL.Modeles
+{
+    public class Department
+    {
+        public int ID { get; set; }
+
+        [Required(ErrorMessage ="Name Is Required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Code Is Required")]
+
+        public string Code { get; set; }
+        public DateTime DateOfCreation { get; set; }
+    }
+}
---
+++ Demo.pl/Controllers/DepartmentController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.pl.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        private readonly IDepartmentReposatory _departmentReposatory;
+
+        public DepartmentController(IDepartmentReposatory departmentReposatory)
+        {
+            _departmentReposatory = departmentReposatory;
+        }
+        public IActionResult Index()
+        {
+            var department = _departmentReposatory.GetAll();
+            return View();
+        }
+    }
+}
